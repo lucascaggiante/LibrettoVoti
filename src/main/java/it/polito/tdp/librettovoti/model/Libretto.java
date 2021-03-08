b040690be9b46cf78b1c580eac904479cacc7ac0
lucas <lucas@LAPTOP-7B93ACRT.home-life.hub>
2021-03-08 11:20:07 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -29,5 +29,50 @@ public class Libretto {
 		return s;
 		
 	}
-
+/*
+	public void stampaVotiUguali (int punteggio) {
+		//il libretto stampa da solo i voti
+		
+		//E' il metodo da pigri
+	}
+	
+	public String votiUguali (int punteggio) {
+		//calcola una stringa che contiene i voti
+		//sarà poi il chiamante a stamparli
+		
+		//tutti i corsi in che senso? solo il nome del corso o anche voto o anche data?
+	}
+	
+	public List<Voto> votiUguali2(int punteggio) {
+		//restituisce solo i voti uguali al criterio
+	}
+	
+	public Libretto votiUguali ( int punteggio) {
+		//restituisce un libretto che ha i voti tutti uguali (che ho scelto)
+		//rimane lo stesso un libretto, infatti posso effetturare tutte le operazioni
+	}
+	*/
+	
+	public List<Voto> listaVotiUguali(int punteggio) {
+		//restituisce solo i voti uguali al criterio
+		
+		List<Voto> risultato = new ArrayList<>();
+		for (Voto v : this.voti ) {
+			if (v.getVoto() == punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
+	
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato = new Libretto();
+		for (Voto v : this.voti) {
+			if (v.getVoto()==punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
+	
 }
